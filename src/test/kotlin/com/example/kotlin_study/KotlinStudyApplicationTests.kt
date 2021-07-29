79272decf3c05bbd1e53b7d3ba9308e4ff1cfa0f
Gyunny <wjdrbs966@naver.com>
2021-07-29 22:58:30 +0900

    initial commit

---
+++ src/test/kotlin/com/example/kotlin_study/KotlinStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin_study
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
